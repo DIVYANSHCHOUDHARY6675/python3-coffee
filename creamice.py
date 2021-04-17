5f333989097062657e5641e345bf6b2f7ff221bf
DIVYANSHCHOUDHARY6675 <76527055+DIVYANSHCHOUDHARY6675@users.noreply.github.com>
2021-04-17 15:42:16 +0530

    Update creamice.py

--- creamice.py
+++ creamice.py
@@ -1,10 +1,25 @@
 import plotly.express as px
 import csv
-with open ("icecold.csv") as dew:
-    df=csv.DictReader(dew)
-    fig=px.scatter(df,x="Coffee in ml",y="sleep in hours")
-    fig.show()
-    
+import numpy as np 
+
+def getDataSource(data_path):
+    icecream=[]
+    coldrink=[]
+    temprature=[]
+    with open (data_path) as dew:
+        csvreader=csv.DictReader(dew)
+        for i in csvreader:
+            icecream.append(float(i["Ice-cream Sales( ₹ )"]))
+            temprature.append(float(i["Temperature"]))
+    return {"x":temprature,"y":icecream}
+def find (data_source):
+    correlation=np.corrcoef(data_source["x"],data_source["y"])
+    print("the correlation of the data source is"+str(correlation))
+def setup ():
+   source = getDataSource("icecold.csv")
+   find(source)
+setup()
+
 
 
 
